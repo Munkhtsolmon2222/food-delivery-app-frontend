8de415ef3a9271662a8a12270fcc842c0c926406
MunkhtTsolmon <monkhtsolmonmuzo@gmail.com>
2025-01-15 21:05:40 +0800

    up

--- src/app/admin/(components)/categories.tsx
+++ src/app/admin/(components)/categories.tsx
@@ -1,5 +1,16 @@
 "use client";
+import { Badge } from "@/components/ui/badge";
 import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface Category {
   _id: string;
@@ -8,31 +19,30 @@ interface Category {
 
 export function Categories() {
   const [categories, setCategories] = useState<Category[]>([]);
-
+  const [value, setValue] = useState<string>();
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/food-category/");
+      if (!res.ok) throw new Error("Failed to fetch categories");
+      const resJson = await res.json();
+      setCategories(resJson?.data || []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("http://localhost:4000/food-category/");
-        if (!res.ok) throw new Error("Failed to fetch categories");
-        const resJson = await res.json();
-        setCategories(resJson?.data || []);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchData();
   }, []);
 
   console.log(categories);
 
-  const addCategory = async () => {
-    const newName = prompt("Enter new category name");
-    if (!newName) return;
+  const addCategory = async (value: any) => {
+    if (!value) return;
 
     try {
       const response = await fetch("http://localhost:4000/food-category/", {
         method: "POST",
-        body: JSON.stringify({ categoryName: newName }),
+        body: JSON.stringify({ categoryName: value }),
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
@@ -51,13 +61,66 @@ export function Categories() {
     }
   };
 
+  const handleChanger = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  // const deleteCategory = async (id: any) => {
+  //   try {
+  //     await fetch(`http://localhost:4000/food-category/${id}`, {
+  //       method: "DELETE",
+  //     });
+  //   } catch (e) {
+  //     alert(e);
+  //   }
+  //   fetchData();
+  // };
   return (
-    <div>
-      <h2>Categories</h2>
-      {categories?.map((category) => (
-        <div key={category._id}>{category.categoryName}</div>
-      ))}
-      <button onClick={addCategory}>Add Category</button>
+    <div className="rounded-lg bg-white w-[80vw] mt-6 p-4">
+      <h2 className="text-[22px] font-semibold">Dishes category</h2>
+      <div className="mt-5">
+        <Badge variant="outline" className="mr-2">
+          {" "}
+          <h1 className="p-2 font-medium text-[16px]">All dishes</h1>
+        </Badge>
+        {categories?.map((category) => (
+          <Badge key={category._id} variant="outline" className="mr-2">
+            <h1 className="p-2 font-medium text-[16px]">
+              {category.categoryName}
+            </h1>
+          </Badge>
+        ))}
+        <Dialog>
+          <DialogTrigger>
+            <div className="bg-[#EF4444] rounded-full w-[36px] h-[36px] content-center">
+              <p className="text-white">+</p>
+            </div>
+          </DialogTrigger>
+          <DialogContent className="w-[460px] h-[300px]">
+            <DialogHeader>
+              <DialogTitle>Add new category</DialogTitle>
+              <div className="pt-12">
+                <p>Category name</p>
+                <Input
+                  className="border-1px rounded-md h-[40px] w-[400px]"
+                  onChange={handleChanger}
+                  placeholder="Type category name..."
+                />
+              </div>
+            </DialogHeader>
+            <DialogClose asChild>
+              <Button
+                onClick={() => {
+                  addCategory(value);
+                }}
+                className="w-[40%] ml-[55%]"
+              >
+                Add category
+              </Button>
+            </DialogClose>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   );
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,20 @@
 import { Categories } from "./(components)/categories";
+import { Navigation } from "./(components)/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
-    <>
-      <Categories />
-    </>
+    <div className="flex bg-[#F4F4F5] gap-6 relative">
+      <Navigation />
+      <div className="mx-6 mt-6 ">
+        <div className="rounded-full absolute right-8  ">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+        <Categories />
+      </div>
+    </div>
   );
 }
